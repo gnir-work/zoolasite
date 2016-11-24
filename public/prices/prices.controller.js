ac794c3b4775a2eec001a331cb3c5ab4efc633e8
nir geller <nir@debian.nir_debian>
2016-11-23 19:43:47 -0500

    Update - prices list

--- public/prices/prices.controller.js
+++ public/prices/prices.controller.js
@@ -5,71 +5,45 @@ angular.module('prices').
 
         this.cuts = [{
             cut: 'Blow Dry',
-            price: "30"
+            price: "30+"
         },{
             cut: 'Men’s Cut',
-            price: '25'
+            price: '25+'
         },{
             cut: 'Women’s Cut',
-            price: '40'
+            price: '45+'
         },{
             cut: 'Women’s Cut & Blow-Dry',
-            price: '65'
+            price: '65+'
         },{
             cut: 'Colour',
-            price: '45'
+            price: '45+'
         },{
-            cut: 'Colouring & Cut',
-            price: '70'
+            cut: 'Colour & Cut',
+            price: '70+'
         },{
             cut: 'Colour & Cut & Blow-Dry',
-            price: '90'
+            price: '85+'
         },{
             cut: 'Full Highlights',
-            price: '120+'
+            price: '100+'
         },{
             cut: 'Partial Highlights',
             price: '75+'
         },{
             cut: 'Men’s Colour',
-            price: '30'
+            price: '30+'
         },{
             cut: 'Ombré/Balayage',
             price: '100+'
         },{
             cut: 'Hair Treatments',
-            price: '35'
+            price: '20-45'
         },{
             cut: 'Boy’s Cut',
             price: '20'
         },{
             cut: 'Girl’s Cut',
-            price: '30'
+            price: '35'
         }];
-
-        this.deals = [{
-            name: 'Blow Dry Klub: 5 Visits (Wash & Blow-Dry)',
-            price: '15'
-        },{
-            name: 'Women’s Cut, Colour & Blow-Dry',
-            price: '15'
-        },{
-            name: 'Women’s Colour & Blow-Dry',
-            price: '15'
-        },{
-            name: 'Men’s Cut & Color',
-            price: '15'
-        },{
-            name: 'Colour, Partial Highlights, Cut & Blow-Dry',
-            price: '15'
-        },{
-            name: 'Colour, Full Highlights, Cut & Blow-Dry',
-            price: '15'
-        },{
-            name: 'Full Highlights, Cut & Blow-Dry',
-            price: '15'
-        },{
-            name: 'Partial Highlights, Cut & Blow-Dry ',
-            price: '15'
-        }]
 });
