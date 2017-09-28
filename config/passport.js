7a699427c231cdb85bcd679c44f9353fa3aadf16
nir geller <geller.nir@gmail.com>
2017-09-29 02:34:49 +0300

    Added support for hash

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,6 @@
 console.log("this is a test");
 var LocalStrategy = require('passport-local').Strategy;
-
+var hash = require('string-hash');
 
 module.exports = function (passport) {
 
@@ -18,7 +18,7 @@ module.exports = function (passport) {
     passport.use(new LocalStrategy({
             passReqToCallback: true
         }, function (req, username, password, done) {
-            if (username === 'zoola' && password === 'zoolaSalon') {
+            if (username === 'zoola' && hash(password) === 3487765549) {
                 done(null, {username: 'nir'});
             } else {
                 done(null, false);
